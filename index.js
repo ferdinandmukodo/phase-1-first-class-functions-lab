227f187c0523aa5f3f5f17b66daeffa5531d9795
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-08-19 12:43:54 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (arrDrivers) {
+	return arrDrivers.slice(0,2);
+	}
+
+const returnLastTwoDrivers = function (arrDrivers) {
+	return arrDrivers.slice(-2);
+	}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function fareDoubler(fare) {
+	return fare * 2;
+	}
+
+function fareTripler(fare) {
+	return fare * 3;
+	}
+
+
+const createFareMultiplier = function (multiplyFare) {
+	return function (multiplier) {
+		return multiplyFare * multiplier;
+		};
+	}
+
+const selectDifferentDrivers = function (arrDrivers,selectedDrivers) {
+	return selectedDrivers(arrDrivers);
+	}
